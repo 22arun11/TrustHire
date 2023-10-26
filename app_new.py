106c8d786c8d0223d83987336d306c909645aad4
ArunKumar J <arunkumar22112003@gmail.com>
2023-10-26 20:45:03 +0530

    Update app_new.py

--- app_new.py
+++ app_new.py
@@ -18,7 +18,7 @@ from Courses import ds_course, web_course, android_course, ios_course, uiux_cour
 import pafy
 import plotly.express as px
 import youtube_dl
-
+nltk.download('stopwords')
 import requests
 import io
 import openai
